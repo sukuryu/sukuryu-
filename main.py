3da0343966bbbc436b0f23572f0701d40fe524b4
kikuchi <tomopiyo0001234@yahoo.co.jp>
2016-12-28 12:58:29 +0900

    overlap-add完成

--- main.py
+++ main.py
@@ -11,19 +11,15 @@ elev0Hrtf_L, elev0Hrtf_R = load.load_elev0hrtf()
 sound_data_path = "./test.wav"
 N = 512
 CHANNELS = 2
-position = 18
+position = 45
 #2048
 FFT_size = 2048
 M = 1537
 overLap = 511
-write_size = 1026
-volume = 2
-
+volume = 3
 index = 0
-history_L = numpy.zeros(overLap, dtype = numpy.int16)
-history_R = numpy.zeros(overLap, dtype = numpy.int16)
-
-buf = numpy.empty((0, 2), dtype = numpy.int16)
+history_L = numpy.zeros(overLap, dtype = numpy.float64)
+history_R = numpy.zeros(overLap, dtype = numpy.float64)
 
 rate, sound_data = scw.read(sound_data_path)
 
@@ -33,14 +29,20 @@ stream = p.open(format = 8,
                 rate = rate,
                 output = True)
 
+buf = numpy.empty((0, 2), dtype = numpy.int16)
+
+#-----------------------------------------------------------------
+
+#畳み込み処理
 def convolution(data, hrtf):
     spectrum = numpy.fft.fft(data, n = FFT_size)
     hrtf_fft = numpy.fft.fft(hrtf, n = FFT_size)
     add = spectrum * hrtf_fft
-    result = numpy.fft.ifft(add, n = M)
-    return_data = result.real.astype(numpy.int16) * volume
-    return return_data[:write_size], return_data[write_size:]
+    result = numpy.fft.ifft(add, n = FFT_size)
+    return_data = result.real * volume
+    return return_data[:M], return_data[M:]
 
+#リアルタイム処理部分
 while(sound_data[index:, 0].size > M):
     result_data = numpy.empty((0, 2), dtype = numpy.int16)
     tmp_conv_L, add_L = convolution(sound_data[index:index + M, 0], elev0Hrtf_L[position])
@@ -53,10 +55,11 @@ while(sound_data[index:, 0].size > M):
     history_R = add_R
 
     for i in range(tmp_conv_L.size):
-        result_data = numpy.append(result_data, numpy.array([tmp_conv_L[i], tmp_conv_R[i]]))
+        result_data = numpy.append(result_data, numpy.array([[int(tmp_conv_L[i]), int(tmp_conv_R[i])]], dtype = numpy.int16), axis = 0)
 
     stream.write(bytes(result_data))
-    index += write_size
+    index += M
 
+#ストリームを閉じる
 stream.close()
 p.terminate()
--- test.py
+++ test.py
@@ -1,127 +1,103 @@
-import wave
+
+import matplotlib.pyplot as plt
+import numpy
+import scipy.io.wavfile as scw
 import pyaudio
+import wave
+import load_hrtf
 
-hrtf_L = {}
-hrtf_R = {}
+load = load_hrtf.load_hrtf()
+elev0Hrtf_L, elev0Hrtf_R = load.load_elev0hrtf()
 
-#--------------L_Load-----------------------------------
-for i in range(72):
-    str_i = str(i * 5)
-
-    if len(str_i) < 2:
-        str_i = "00" + str_i
-    elif len(str_i) < 3:
-        str_i = "0" + str_i
-
-    filename = "L0e" + str_i + "a.dat"
-    filepath = "../hrtfs/elev0/" + filename
-    test = open(filepath, "r").read().split("\n")
-
-    data = []
-
-    for item in test:
-        if item != '':
-            data.append(float(item))
-
-    hrtf_L[i] = data
-
-#---------------R_Load----------------------------
-for i in range(72):
-    str_i = str(i * 5)
-
-    if len(str_i) < 2:
-        str_i = "00" + str_i
-    elif len(str_i) < 3:
-        str_i = "0" + str_i
-
-    filename = "R0e" + str_i + "a.dat"
-    filepath = "../hrtfs/elev0/" + filename
-    test = open(filepath, "r").read().split("\n")
-
-    data = []
-
-    for item in test:
-        if item != '':
-            data.append(float(item))
-
-    hrtf_R[i] = data
-
-#------------変数------------------------
 sound_data_path = "./test.wav"
+N = 512
+CHANNELS = 2
+position = 18
+FFT_size = 2048
+M = 1537
+overLap = 511
+write_size = 1026
 
-def play():
-    wf = wave.open(sound_data_path, "r")
+count = 0
 
+def convolutionL(data):
+    #与えられたデータに窓をかける
+    #window = numpy.hanning(N)
+    #dt = data * window
 
+    #fft
+    spectrum = numpy.fft.fft(data, n = FFT_size)
+
+    #---------hrtfを足し合わせる--------------------
+
+    hrtf_fft = numpy.fft.fft(elev0Hrtf_L[position], n = FFT_size)
+    #print(hrtf_fft)
+    result = spectrum * hrtf_fft
+
+    #ifft
+    ret_dt = numpy.fft.ifft(result, n = M)
+
+    return ret_dt.real
+
+def convolutionR(data):
+    #与えられたデータに窓をかける
+    #window = numpy.hanning(N)
+    #dt = data * window
+
+    #fft
+    spectrum = numpy.fft.fft(data, n = FFT_size)
+
+    #---------hrtfを足し合わせる--------------------
+
+    hrtf_fft = numpy.fft.fft(elev0Hrtf_R[position], n = FFT_size)
+    #print(hrtf_fft)
+    result = spectrum * hrtf_fft
+
+    #ifft
+    ret_dt = numpy.fft.ifft(result, n = M)
+
+    return ret_dt.real
+
+def play(sound_data):
     p = pyaudio.PyAudio()
-
-    stream = p.open(format = p.get_format_from_width(wf.getsampwidth()),
-                    channels = wf.getnchannels(),
-                    rate = wf.getframerate(),
+    stream = p.open(format = 8,
+                    channels = CHANNELS,
+                    rate = rate,
                     output = True)
 
-    chunk = 512
+    #while(sound_data[index:, 0].size > N):
+    #    stream.write(bytes(sound_data[index:index + N]))
+    #    index += N
 
-    data = wf.readframes(chunk)
-
-    print(p.get_format_from_width(wf.getsampwidth()))
-
-    while data != b'':
-        stream.write(data)
-        data = wf.readframes(chunk)
+    stream.write(bytes(sound_data))
 
     stream.close()
-    p.terminate
+    p.terminate()
 
-play()
+rate, data = scw.read(sound_data_path)
+
+L_data = data[:, 0]
+R_data = data[:, 1]
+
+ret_data_L = numpy.zeros(L_data.size)
+ret_data_R = numpy.zeros(R_data.size)
+
+index = 0
+
+while(ret_data_L[index:].size > M):
+    ret_data_L[index:index + M] += convolutionL(L_data[index:index + M])
+    ret_data_R[index:index + M] += convolutionR(R_data[index:index + M])
+
+    index += write_size
+
+result_data = numpy.empty((0, 2), numpy.int16)
+
+for i in range(L_data.size):
+    result_data = numpy.append(result_data, numpy.array([[int(ret_data_L[i]), int(ret_data_R[i])]]).astype(numpy.int16), axis=0)
 
 
-
-
-#--------------------------
-
-#--------------L_Load-----------------------------------
-for i in range(72):
-    str_i = str(i * 5)
-
-    if len(str_i) < 2:
-        str_i = "00" + str_i
-    elif len(str_i) < 3:
-        str_i = "0" + str_i
-
-    filename = "L0e" + str_i + "a.dat"
-    filepath = "../hrtfs/elev0/" + filename
-    test = open(filepath, "r").read().split("\n")
-
-    data = []
-
-    for item in test:
-        if item != '':
-            data.append(float(item))
-
-    hrtf_L[i] = data
-
-#---------------R_Load----------------------------
-for i in range(72):
-    str_i = str(i * 5)
-
-    if len(str_i) < 2:
-        str_i = "00" + str_i
-    elif len(str_i) < 3:
-        str_i = "0" + str_i
-
-    filename = "R0e" + str_i + "a.dat"
-    filepath = "../hrtfs/elev0/" + filename
-    test = open(filepath, "r").read().split("\n")
-
-    data = []
-
-    for item in test:
-        if item != '':
-            data.append(float(item))
-
-    hrtf_R[i] = data
-
-#hrtf_data = hrtf_R[0]
-
-#-----------------処理-------------------
+plt.plot(result_data[:, 0])
+plt.show()
+#再生部分作成
+#rate = int(rate * 1.2)
+play(result_data)
